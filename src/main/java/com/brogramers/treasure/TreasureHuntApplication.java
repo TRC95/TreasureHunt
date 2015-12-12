5695596b07c18f11e5f85c398043f35ea223794e
danvatamaniuc <da.vatamaniuc@gmail.com>
2015-12-12 14:25:24 +0200

    Uploaded base project
    Hermes Hackathon project.

---
+++ src/main/java/com/brogramers/treasure/TreasureHuntApplication.java
@@ -0,0 +1,12 @@
+package com.brogramers.treasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureHuntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureHuntApplication.class, args);
+    }
+}
