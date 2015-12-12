4a57f3286f645f681118e3dc887a7779c8b0ba2b
Timis Razvan <tymysrazvan@gmail.com>
2015-12-12 15:12:16 +0200

    Add Hint java Interface

--- HuntCommon/src/model/Hint.java
+++ HuntCommon/src/model/Hint.java
@@ -2,6 +2,12 @@ package model;
 
 /**
  * Created by 1 on 12/12/2015.
+ * In
  */
 public interface Hint {
+    Integer getId();
+    Integer getPossition();
+    String getUrlImage();
+    String getMessage();
+    Integer getCode();
 }
