1cede4e9794539aa92e37afa628f825334df307e
TRC95 <cristi95gabriel@yahoo.com>
2015-12-12 15:47:03 +0200

    Added User interface

--- HuntCommon/src/model/User.java
+++ HuntCommon/src/model/User.java
@@ -4,4 +4,12 @@ package model;
  * Created by 1 on 12/12/2015.
  */
 public interface User {
+	
+	public Integer getId();
+	public String getName();
+	public String getEmail();
+	
+	public void setId(Integer id);
+	public void setName(String name);
+	public void setEmail(String emai);
 }
